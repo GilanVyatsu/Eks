dd391ac7356e2306fe92f0cb9d756fdfea8373dc
Dmitriy <Dmitriy@SCORPIO-DM>
2020-07-04 14:58:28 +0300

    Добавьте файлы проекта.

---
+++ Eks/FormLogin.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Eks
+{
+    public partial class FormLogin : Form
+    {
+        public FormLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonRegistr_Click(object sender, EventArgs e)
+        {
+            Form registr = new FormRegistr();
+            registr.Show();
+        }
+
+        private void buttonLogin_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
